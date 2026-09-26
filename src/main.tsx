e9aad48e1aa175c1c95309fc6dd039ead493a5a3
drsalomaons-cell <dr.salomaons@gmail.com>
2026-09-26 01:42:44 -0300

    feat: initialize vConsole mobile diagnostics

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,13 @@
-import React from "react";import{createRoot}from"react-dom/client";import"./styles.css";import App from"./App";createRoot(document.getElementById("root")!).render(<React.StrictMode><App/></React.StrictMode>);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import App from "./App";
+import { initVConsole } from "./lib/vconsole";
+
+initVConsole();
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
