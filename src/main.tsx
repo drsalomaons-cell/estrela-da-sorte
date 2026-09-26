a858b5540884c2cba782ac5dbb6293a243d092c0
drsalomaons-cell <dr.salomaons@gmail.com>
2026-09-26 01:45:05 -0300

    feat: initialize OpenTelemetry alongside vConsole

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
 import { initVConsole } from "./lib/vconsole";
+import { initOpenTelemetry } from "./lib/opentelemetry";
 
 initVConsole();
+initOpenTelemetry();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
